cec44d9f77d83d3c92f9b6f5d39a4a0abe4385b8
nicktheperkins <nicktheperkins@gmail.com>
2021-07-12 18:13:18 -0600

    Added new folders and files

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+// FUNCTION ASSIGNMENT
+//function myFunction() { // Defining a function and naming it
+//    var str = "This text is green!"; // Defining a variable and giving it a string value
+//    var result = str.fontcolor("green"); // Using the fontcolor method on str variable
+//    document.getElementById("Green_Text").innerHTML = result; // Putting the value of result into HTML element with "Green_Text" id
+//}
+// END FUNCTION ASSIGNMENT
+
+// OPERATOR ASSIGNMENT
+function myFunction() {
+    var sentence="I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+// END OPERATOR ASSIGNMENT
